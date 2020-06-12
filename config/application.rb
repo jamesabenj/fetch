1cca4fcb2e7541f17031f00438d87312cf325ab3
jamesabenj <james.abenj@gmail.com>
2020-06-12 14:36:11 -0400

    call load for dotenv

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,13 @@
 require_relative 'boot'
 
+
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 module Fetchapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -13,3 +15,5 @@ module Fetchapp
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+
