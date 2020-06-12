d3a00df027e28cc83a8297bf9b08a2801e0f5312
jamesabenj <james.abenj@gmail.com>
2020-06-12 14:26:38 -0400

    define initial validations for Dog

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,6 @@
 class Dog < ApplicationRecord
+    #belongs_to :user
+    mount_uploaders :images, FileUploader
     validates :name, presence: true
     validates :bio, presence: true 
     validates :breed, presence: true 
