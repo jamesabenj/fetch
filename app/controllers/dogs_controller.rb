f643aedcc6d9bf0a4ef29248d1b64bbc930f8203
jamesabenj <james.abenj@gmail.com>
2020-06-17 12:06:17 -0400

    define routes global_index route

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,43 +1,73 @@
 class DogsController < ApplicationController
-
+    before_action :require_login, except: [:index, :global_index, :show]
+    layout :dogs_layout
     def new
         @dog = Dog.new
+        @breed = Breed.new
+    end 
+
+    def index
+       @user = User.find(params[:user_id])
+       @dogs = @user.dogs
+       if !@dogs.any?
+        @notice = "This user doesn't have any available dogs."
+       end 
+    end 
+
+    def global_index
+        @dogs = Dog.search(params[:search])
+        if params[:search] && !Breed.find_by(name: params[:search])
+            @error = "No dogs match that breed, showing all dogs"
+        end 
     end 
 
     def create
         @dog = Dog.new(dog_params)
+        breed = Breed.find_or_create_by(breed_params)
+        @dog.breed = breed
+        current_user.dogs << @dog
         if @dog.save
-            #byebug
-            redirect_to dog_path(@dog)
+            redirect_to user_dog_path(current_user, @dog)
         else 
             render :new
         end
     end 
 
-    def index
-        @dogs = Dog.all
-    end 
-
     def show
-        @dog = Dog.find(params[:id])
-        #byebug
+        find_dog
     end 
 
     def edit
-        @dog = Dog.find(params[:id])
+        find_dog
     end 
 
     def update
-        @dog = Dog.find(params[:id])
-        @dog.update(dog_params)
-        @dog.save
-        redirect_to dog_path(@dog)
+        find_dog
+        if @dog.user == current_user
+            @dog.update(dog_params)
+            if @dog.valid?
+                redirect_to user_dog_path(current_user, @dog)
+            else 
+                render :edit
+            end
+        else 
+            redirect_to profile_path(current_user)
+        end 
     end 
 
-    def delete
-    end 
+    def destroy
+        find_dog
+        @dog.destroy
+        redirect_to user_dogs_path
+    end
+
+    private
 
     def dog_params
-        params.require(:dog).permit(:name, :bio, :adopted, :breed, {images: []})
+        params.require(:dog).permit(:name, :bio, :adopted, {images: []})
+    end 
+
+    def breed_params
+        params.require(:breed).permit(:name)
     end 
 end
