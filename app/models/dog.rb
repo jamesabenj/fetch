e2e57b92e0696e67797884abb14a027d389fd312
jamesabenj <james.abenj@gmail.com>
2020-06-17 12:11:06 -0400

    define methods for fomatting images and breed search

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,35 @@
 class Dog < ApplicationRecord
-    #belongs_to :user
+    has_many :favorites
+    has_many :followers, through: :favorites
+    belongs_to :user
+    belongs_to :breed
     mount_uploaders :images, FileUploader
-    validates :name, presence: true
-    validates :bio, presence: true 
-    validates :breed, presence: true 
+    serialize :images, JSON
+    validates_presence_of :name
+    validates_presence_of :bio
+
+
+    
+    def formatted_img_urls
+        img_urls = []
+        self.images.each do |image|
+            new_url = image.url.gsub("%5B%22", "").gsub("%22%5D", "")
+            img_urls << new_url
+        end
+        img_urls
+    end 
+
+    def self.search(search)
+        if search 
+            search.capitalize!
+            breed = Breed.find_by(name: search)
+            if breed
+                self.where(breed_id: breed)
+            else 
+                Dog.all
+            end 
+        else
+            Dog.all
+        end
+    end 
 end
