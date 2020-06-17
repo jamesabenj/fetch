5cda32243ce19b2443c701a7904382402aa12703
jamesabenj <james.abenj@gmail.com>
2020-06-17 12:01:25 -0400

    define helper methods for finding models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end 
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end 
+
+  def require_login
+    if current_user
+      current_user
+    else
+        redirect_to '/'
+    end
+  end
+
+  def dogs_layout
+    current_user ? "user" : "general" 
+  end 
 end
 
