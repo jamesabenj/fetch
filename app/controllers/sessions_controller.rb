ae96120f63c4765e17fc9d40a687c763012af285
jamesabenj <james.abenj@gmail.com>
2020-06-17 12:08:16 -0400

    add error message for invalid credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,38 @@
 
 class SessionsController < ApplicationController
+    layout :dogs_layout
 
-  def authcreate
-    User.from_omniauth(auth)
-  end
-
-    def new 
-      @user = User.new
+    def home 
+      if current_user
+        redirect_to profile_path(current_user)
+      end 
     end 
+
+    def authcreate
+      data = auth.info
+      @user = User.find_or_create_by(username: data['email'])
+      if !@user.password
+        @user.password = SecureRandom.hex
+      end
+      @user.save
+      session[:user_id] = @user.id
+      #byebug
+      redirect_to profile_path(@user)
+    end
   
     def create
       @user = User.find_by(username: params[:user][:username])
       if @user && @user.authenticate(params[:user][:password])
           session[:user_id] = @user.id
-          redirect_to user_path(@user)
+          redirect_to profile_path(@user)
       else
+          @error = "Invalid credentials."
           render :new
       end
     end
   
     def destroy
-      session.delete :user_id
+      session.clear
       redirect_to '/'
     end
 
