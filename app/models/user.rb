a3ac0abca713ff9c864e8d3c86890e1ef6a3d919
jamesabenj <james.abenj@gmail.com>
2020-06-17 12:12:34 -0400

    add validation for unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,6 @@
 class User < ApplicationRecord
-  
+  has_many :dogs
+  has_secure_password
   validates :username, presence: true 
-
-  def self.find_or_create_by_auth(access_token)
-    data = access_token.info
-    @user = User.where(email: data['email']).first
-    @user.uid = data
-    # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data['name'],
-    #        email: data['email'],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
-    @user
-  end
+  validates :username, uniqueness: true
 end
