4010544458b800955cda00e960f733474db0138f
jamesabenj <james.abenj@gmail.com>
2020-06-23 13:10:35 -0400

    remove redundant find_by method

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,15 +1,4 @@
 class Follower < ApplicationRecord
     has_many :favorites
     has_many :dogs, through: :favorites
-
-
-    def get_dogs
-        dog_ids = dog = self.favorites.map do |fav|
-            fav.dog_id
-        end 
-        @dogs = dog_ids.map do |id|
-            Dog.find(id) 
-        end 
-        @dogs
-    end 
 end
