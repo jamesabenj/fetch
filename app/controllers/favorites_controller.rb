379bde1d25b6919053ab473b2b126e779c2bcf1e
jamesabenj <james.abenj@gmail.com>
2020-06-23 12:57:00 -0400

    remove redundant find_by method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,6 @@
 class FavoritesController < ApplicationController
     layout 'user'
     def create
-        #byebug
         fav = Favorite.new
         find_dog
         follower = Follower.find_or_create_by(id: current_user.id)
@@ -13,9 +12,7 @@ class FavoritesController < ApplicationController
 
     def index
         follower = Follower.find_by(id: current_user.id)
-        if follower
-            @dogs = follower.get_dogs
-        end 
+        @dogs = follower.dogs
         if !follower || !@dogs.any?
             @notice = "You don't have any favorites."
         end
